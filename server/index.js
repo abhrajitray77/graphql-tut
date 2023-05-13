c51b8c818c02edde7a495a7d9791ba881a4ab91a
abhrajitray77 <abhrajitray77@gmail.com>
2023-05-13 16:02:05 +0530

    schema made

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const port = process.env.PORT || 5000;
-const schema = require('./schema/schema');
+const schema = require('../schema/schema');
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
